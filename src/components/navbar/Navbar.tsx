611abe019e9e4dfe34fed64c8bad9960452a2ddf
Norm Copeland <norm@goodmanrobot.com>
2022-03-21 09:21:04 -0400

    initial

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,49 @@
+import {
+  ChartBarIcon,
+  CogIcon,
+  InformationCircleIcon,
+  RefreshIcon,
+} from '@heroicons/react/outline'
+import { GAME_TITLE } from '../../constants/strings'
+
+type Props = {
+  setIsInfoModalOpen: (value: boolean) => void
+  setIsStatsModalOpen: (value: boolean) => void
+  setIsSettingsModalOpen: (value: boolean) => void
+}
+
+export const Navbar = ({
+  setIsInfoModalOpen,
+  setIsStatsModalOpen,
+  setIsSettingsModalOpen,
+}: Props) => {
+  return (
+    <div className="navbar">
+      <div className="navbar-content px-5">
+        <div className="right-icons">
+          <InformationCircleIcon
+            className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
+            onClick={() => setIsInfoModalOpen(true)}
+          />
+          <a href="/">
+            <RefreshIcon className="h-6 w-6 cursor-pointer dark:stroke-white" />
+          </a>
+        </div>
+
+        <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>
+
+        <div className="right-icons">
+          <ChartBarIcon
+            className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+            onClick={() => setIsStatsModalOpen(true)}
+          />
+          <CogIcon
+            className="h-6 w-6 cursor-pointer dark:stroke-white"
+            onClick={() => setIsSettingsModalOpen(true)}
+          />
+        </div>
+      </div>
+      <hr></hr>
+    </div>
+  )
+}
